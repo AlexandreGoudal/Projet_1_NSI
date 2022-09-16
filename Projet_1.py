7d61c1c57af00aee7df85703b027fda2159aa680
AlexandreGoudal <113675095+AlexandreGoudal@users.noreply.github.com>
2022-09-16 11:48:00 +0200

    Update Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -2,11 +2,15 @@ nombre_initial = float (input ("Veuillez entrer le nombre à transformer : ")
 base_initiale = float (input ("Veuillez entrer la base du nombre : "))
 base_finale = float (input ("Veuillez entrer la base souhaitée : "))
 
-def 
+def base_10_2 (nombre_initial, base_initiale)
+    for i in range 
+    q, r = divmod(nombre_initial, 2)
+    
 
 
 
-                        
+
+
 
 print ("Le nombre " str (nombre_inital) "en base " str (base_initiale)\
        + "vaut " str (nombre_final) "en base " str (base_finale))
