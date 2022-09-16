f0cdbe4f3984fa6b2c65c25a600b08d403096751
AlexandreGoudal <113675095+AlexandreGoudal@users.noreply.github.com>
2022-09-16 11:19:44 +0200

    Add files via upload

---
+++ Projet_1.py
@@ -0,0 +1,9 @@
+nombre_initial = float (input ("Veuillez entrer le nombre
+
+
+
+
+
+
+print ("Le nombre " str (nombre_inital) "en base " str (base_initiale)\
+       + "vaut " str (nombre_final) "en base " str (base_finale))
