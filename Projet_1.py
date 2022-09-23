8cd288f1cf8951fef3badbb8492342ff07a4862a
AlexandreGoudal <113675095+AlexandreGoudal@users.noreply.github.com>
2022-09-23 10:44:11 +0200

    Update Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -1,20 +1,23 @@
-nombre_initial = float (input ("Veuillez entrer le nombre à transformer : ")
-base_initiale = float (input ("Veuillez entrer la base du nombre (Les seules bases traitées sont 2, 10 et 16) : "))
-base_finale = float (input ("Veuillez entrer la base souhaitée (Les seules bases traitées sont 2,10 et 16)  : "))
+nombre_initial = int (input ("Veuillez entrer le nombre à transformer : ")
+base_initiale = int (input ("Veuillez entrer la base du nombre (Les seules bases traitées sont 2, 10 et 16) : "))
+base_finale = int (input ("Veuillez entrer la base souhaitée (Les seules bases traitées sont 2,10 et 16)  : "))
 
 if base_initial ! 2 or base_initale ! 10 or base_initiale ! 16 :
     print  ("La base du nombre que vous avez entrée n'est pas traitée par ce programme ")
 
 def base_10_2 (nombre_initial, base_initiale)
     while r ! 0 and q ! 0
-        nombre_initial = divmod (nombre_initial, 2)
-        append (r)
+        nombre_final = []
+        nombre_initial = divmod (nombre_initial, base_initiale)
+        nombre_final.append (r)
+    return nombre_final
     
-        
+
         
                         
         
     
+
 
 
 
@@ -25,7 +28,7 @@ print ("Le nombre "\
        + " en base "\
        + str (base_initiale)\
        + " vaut "\
-       + str (nombre_final)\
+       + str (base_10_2 (nombre_initial, base_initiale))\
        + " en base "\
        + str (base_finale)\
        + ".")
