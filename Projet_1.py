8bd7b0cce7612ed20abe8fe297205ba5e16a95a6
cyprienlao <113675006+cyprienlao@users.noreply.github.com>
2022-09-23 11:44:58 +0200

    Create Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -1,4 +1,5 @@
-nombre_initial = input ("Veuillez entrez l'entier à traiter : ")
+# -*- coding: cp1252 -*-
+nombre_initial = int (input ("Veuillez entrez l'entier à traiter : "))
 
 if type (nombre_initial) != int :
     print ("Cette valeur n'est pas traitée par le programme, nous nous excusons pour la gêne occasionée.")
@@ -10,14 +11,21 @@ if base_initiale != 2 and base_initiale != 10 and base_initiale != 16 :
     print  ("La base du nombre que vous avez entrée n'est pas traitée par ce programme ")
     exit()
 
-base_finale = int (input ("Veuillez entrer la base souhaitée (Les seules bases traitées sont 2,10 et 16)  : "))
+base_finale = int (input ("Veuillez entrer la base souhaitée (Les seules bases traitées sont 2, 10 et 16)  : "))
 
 if base_finale != 2 and base_initiale != 10 and base_initiale != 16 :
     print  ("La base du nombre que vous avez entrée n'est pas traitée par ce programme ")
     exit()
 
+def conversion (nombre_initial, base_initiale, base_finale):
+    if base_initiale == 10 and base_finale == 2:
+        nombre_final = bin (nombre_initial)
+        return nombre_final
+    elif base_initiale == 10 and base_finale == 16:
+        nombre_final = hex (nombre_initial)
+        return nombre_final
 
-def base_10_2 (nombre_initial, base_initiale) :
+def base (nombre_initial, base_initiale) :
     r = 1
     q = 1
     while r != 0 and q != 0 :
@@ -25,14 +33,7 @@ def base_10_2 (nombre_initial, base_initiale) :
         nombre_initial = divmod (nombre_initial, base_initiale)
         nombre_final.append (r)
     return nombre_final
-    
-
-
-        
-                        
-        
-    
-
+    pass
 
 
 
@@ -43,7 +44,8 @@ print ("Le nombre "\
        + " en base "\
        + str (base_initiale)\
        + " vaut "\
-       + str (base_10_2 (nombre_initial, base_initiale))\
+       + str (conversion (nombre_initial, base_initiale, base_finale))\
        + " en base "\
        + str (base_finale)\
        + ".")
+
