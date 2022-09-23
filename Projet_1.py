60eb2d3974e4bb60d9370acb2ab1a317cab70820
AlexandreGoudal <113675095+AlexandreGoudal@users.noreply.github.com>
2022-09-23 10:56:07 +0200

    Update Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -2,7 +2,7 @@ nombre_initial = int (input ("Veuillez entrer le nombre à transformer : "))
 base_initiale = int (input ("Veuillez entrer la base du nombre (Les seules bases traitées sont 2, 10 et 16) : "))
 base_finale = int (input ("Veuillez entrer la base souhaitée (Les seules bases traitées sont 2,10 et 16)  : "))
 
-if base_initiale != 2 and base_initale != 10 and base_initiale != 16 :
+if base_initiale != 2 and base_initiale != 10 and base_initiale != 16 :
     print  ("La base du nombre que vous avez entrée n'est pas traitée par ce programme ")
 
 def base_10_2 (nombre_initial, base_initiale) :
