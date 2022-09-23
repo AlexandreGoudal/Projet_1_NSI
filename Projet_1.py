399df9e16b8ff8dc14e9f3d0b6791cc9152f3f8e
cyprienlao <113675006+cyprienlao@users.noreply.github.com>
2022-09-23 11:00:33 +0200

    Create Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -1,23 +1,29 @@
-nombre_initial = int (input ("Veuillez entrer le nombre à transformer : "))                
+nombre_initial = int (input ("Veuillez entrez l'entier à traiter : "))
+
+if nombre_initial != int :
+    print ("Cette valeur n'est pas traitée par le programme, nous nous excusons pour la gêne occasionée.")
+    
+
 base_initiale = int (input ("Veuillez entrer la base du nombre (Les seules bases traitées sont 2, 10 et 16) : "))
 base_finale = int (input ("Veuillez entrer la base souhaitée (Les seules bases traitées sont 2,10 et 16)  : "))
 
-if base_initiale != 2 and base_initiale != 10 and base_initiale != 16 :
+
+
+if base_initiale != 2 and base_initale != 10 and base_initiale != 16 :
     print  ("La base du nombre que vous avez entrée n'est pas traitée par ce programme ")
+
+
 
 def base_10_2 (nombre_initial, base_initiale) :
     while r != 0 and q != 0 :
-        nombre_final = []
-        nombre_initial = divmod (nombre_initial, base_initiale)
-        nombre_final.append (r)
-    return nombre_final
+        nombre_initial = divmod (nombre_initial, 2)
+        append (r)
     
-
+        
         
                         
         
     
-
 
 
 
@@ -28,7 +34,7 @@ print ("Le nombre "\
        + " en base "\
        + str (base_initiale)\
        + " vaut "\
-       + str (base_10_2 (nombre_initial, base_initiale))\
+       + str (nombre_final)\
        + " en base "\
        + str (base_finale)\
        + ".")
