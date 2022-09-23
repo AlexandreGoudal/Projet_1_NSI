e71a17393dd56d765caedf69094d2e46146cab91
AlexandreGoudal <113675095+AlexandreGoudal@users.noreply.github.com>
2022-09-23 11:04:26 +0200

    Update Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -2,28 +2,40 @@ nombre_initial = int (input ("Veuillez entrez l'entier à traiter : "))
 
 if nombre_initial != int :
     print ("Cette valeur n'est pas traitée par le programme, nous nous excusons pour la gêne occasionée.")
-    
+    exit ()
 
 base_initiale = int (input ("Veuillez entrer la base du nombre (Les seules bases traitées sont 2, 10 et 16) : "))
+
+if base_initiale != int :
+    print ("Cette valeur n'est pas traitée par le programme, nous nous excusons pour la gêne occasionée.")
+    exit()
+
 base_finale = int (input ("Veuillez entrer la base souhaitée (Les seules bases traitées sont 2,10 et 16)  : "))
 
+if base_finale != int :
+    print ("Cette valeur n'est pas traitée par le programme, nous nous excusons pour la gêne occasionée.")
+    exit()
 
 
-if base_initiale != 2 and base_initale != 10 and base_initiale != 16 :
+if base_initiale != 2 and base_initiale != 10 and base_initiale != 16 :
     print  ("La base du nombre que vous avez entrée n'est pas traitée par ce programme ")
-
-
+    exit()
 
 def base_10_2 (nombre_initial, base_initiale) :
+    r = 1
+    q = 1
     while r != 0 and q != 0 :
-        nombre_initial = divmod (nombre_initial, 2)
-        append (r)
+        nombre_final = []
+        nombre_initial = divmod (nombre_initial, base_initiale)
+        nombre_final.append (r)
+    return nombre_final
     
-        
+
         
                         
         
     
+
 
 
 
@@ -34,7 +46,7 @@ print ("Le nombre "\
        + " en base "\
        + str (base_initiale)\
        + " vaut "\
-       + str (nombre_final)\
+       + str (base_10_2 (nombre_initial, base_initiale))\
        + " en base "\
        + str (base_finale)\
        + ".")
