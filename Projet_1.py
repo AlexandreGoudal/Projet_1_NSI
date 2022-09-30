c9f4bb368f70f75f5e295298332adc42b222daf2
cyprienlao <113675006+cyprienlao@users.noreply.github.com>
2022-09-30 11:00:33 +0200

    Update Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -1,5 +1,5 @@
 # -*- coding: cp1252 -*-
-nombre_initial = input ("Veuillez entrez l'entier à traiter : ")
+nombre_initial = input ("Veuillez entrez l'entier naturel à traiter : ")
 
 if type (nombre_initial) == float:
     print ("Cette valeur n'est pas traité par ce programme.")
@@ -11,11 +11,19 @@ if base_initiale != 2 and base_initiale != 10 and base_initiale != 16 :
     print  ("La base du nombre que vous avez entrée n'est pas traitée par ce programme ")
     exit()
 
+
 base_finale = int (input ("Veuillez entrer la base souhaitée (Les seules bases traitées sont 2, 10 et 16)  : "))
 
 if base_finale != 2 and base_initiale != 10 and base_initiale != 16 :
     print  ("La base du nombre que vous avez entrée n'est pas traitée par ce programme ")
     exit()
+
+def calcul_nbits (nombre_initial, base_initiale, base_finale):
+    if base_initiale == 10 and base_finale == 2:
+        nbits = 0
+    while abs (nombre_initial) > 2**nbits:
+        nbits = nbits + 1
+    return nbits + 1
 
 def conversion (nombre_initial, base_initiale, base_finale):
     if base_initiale == 10 and base_finale == 2:
@@ -24,6 +32,8 @@ def conversion (nombre_initial, base_initiale, base_finale):
     elif base_initiale == 10 and base_finale == 16:
         nombre_final = hex (nombre_initial)
         return nombre_final
+    else :
+        pass
 
 print ("Le nombre "\
        + str (nombre_initial)\
